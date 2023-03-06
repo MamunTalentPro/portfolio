f62ba1b62e71d92d775421a91f4d46719f3346b3
MamunTalentPro <mamun@talentpro.global>
2023-03-06 14:54:58 +0600

    successfully added api for todo list

--- components/DailyStaning.tsx
+++ components/DailyStaning.tsx
@@ -34,7 +34,7 @@ export default function DailyStanding() {
   }
   useEffect(() => {
     fetchTodoList()
-  }, [])
+  })
   return (
     <section id="daily-standing" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
